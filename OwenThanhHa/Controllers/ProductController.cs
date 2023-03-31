3f2e5d8879ce9f41cb9b52f1d67c6aed584535bf
mowtn <bunhinrom79@gmail.com>
2023-03-31 22:07:12 +0700

    add trang chu san pham gio hang

---
+++ OwenThanhHa/Controllers/ProductController.cs
@@ -0,0 +1,35 @@
+﻿using OwenThanhHa.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using OwenThanhHa.Models;
+namespace OwenThanhHa.Controllers
+{
+    public class ProductController : Controller
+    {
+        ThanhHaShopEntities2 obj = new ThanhHaShopEntities2();
+        // GET: Product
+       
+        public ActionResult Product(int id)
+        {
+            var product = obj.Products.Where(m => m.Id == id).FirstOrDefault();
+            var objproducts = obj.Products.Where(m=>m.CategoryId == product.CategoryId).ToList();
+            Product_category objProduct_category = new Product_category();
+            objProduct_category.product = product;
+            objProduct_category.Listproducts = objproducts;
+            return View(objProduct_category);
+        }
+        public ActionResult searchProduct(string searchstring)
+        {
+            var listproducts = new List<Product>();
+            if (!string.IsNullOrEmpty(searchstring))
+            {
+                //lấy ds sản phẩm theo từ khóa tìm kiếm
+                listproducts = obj.Products.Where(m => m.Name.Contains(searchstring)).ToList();
+            }
+            return View(listproducts);
+        }
+    }
+}
