8d553b2293e82ddab9d3077fe2d831d24df7efe6
mowtn <bunhinrom79@gmail.com>
2023-04-22 15:20:55 +0700

    push payment and category for BE

---
+++ OwenThanhHa/Controllers/CategoryController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using OwenThanhHa.Context;
+namespace OwenThanhHa.Controllers
+{
+    public class CategoryController : Controller
+    {
+        // GET: Category
+        public ActionResult CategoryShow(int id)
+        {
+            ThanhHaShopEntities2 ThanhHaEntini = new ThanhHaShopEntities2();
+            var listProduct = ThanhHaEntini.Products.Where(m => m.CategoryId == id).ToList();
+            return View(listProduct);
+        }
+    }
+}
---
+++ OwenThanhHa/Controllers/PaymentController.cs
@@ -0,0 +1,72 @@
+﻿using OwenThanhHa.Context;
+using OwenThanhHa.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OwenThanhHa.Controllers
+{
+    public class PaymentController : Controller
+    {
+        ThanhHaShopEntities2 obj = new ThanhHaShopEntities2();
+        // GET: Payment
+        public ActionResult Index()
+        {
+            if(Session["id"] == null)
+            {
+                return RedirectToAction("Login","Home");
+            }
+            else
+            {
+                //gán dữ liệu cho bảng order
+                Order objOrder = new Order();
+                objOrder.Name = "DonHang" + DateTime.Now.ToString("yyyyMMddHHmmss");
+                objOrder.CreatedOnUTc = DateTime.Now;
+                objOrder.Status = 1;
+                objOrder.UserID = int.Parse(Session["id"].ToString());
+                obj.Orders.Add(objOrder);
+                obj.SaveChanges();
+                int IdOrder = objOrder.Id;
+                //lấy thông tin các từ giỏ hàngh từ biến session
+                var listCart = (List<CartModel>)Session["cart"];
+                //khai báo 1 list để gán list các sản phẩm lấy từ giỏ hàng
+                List<OrderProduct> LstOrderProduct = new List<OrderProduct>();
+                foreach(var item in listCart)
+                {
+                    OrderProduct objorderproduct = new OrderProduct();
+                    objorderproduct.OrderId = IdOrder;
+                    objorderproduct.ProductId = item.product.Id;
+                    objorderproduct.Quantity = item.Quantity;
+                    
+                    LstOrderProduct.Add(objorderproduct);
+                }
+                obj.OrderProducts.AddRange(LstOrderProduct);
+                obj.SaveChanges();
+            }
+            return View();
+        }
+
+        public ActionResult showYourOrder(int id)
+        {
+            Order_ProductOfUser opp = new Order_ProductOfUser();
+            opp.Listorder = obj.Orders.Where(m => m.UserID == id).ToList();
+            opp.Listorderproduct = obj.OrderProducts.ToList();
+            opp.Listproducts = obj.Products.ToList();
+            return View(opp);
+        }
+        public ActionResult changeStatus(int id)
+        {
+
+            Order_ProductOfUser opp = new Order_ProductOfUser();
+            opp.orderitem = obj.Orders.Where(m => m.Id == id).FirstOrDefault();
+            opp.Listorderproduct = obj.OrderProducts.ToList();
+            opp.Listproducts = obj.Products.ToList();
+            return View(opp);
+        }
+
+
+
+    }
+}
